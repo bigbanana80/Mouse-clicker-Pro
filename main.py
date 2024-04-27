e3455f7e68708be6aa70b0dbdc9d206cf1610845
Sepehr Aghajani <75833176+bigbanana80@users.noreply.github.com>
2024-04-27 16:43:15 +0330

    0.0.02 "auto click logic works now, start and stop works as intended"

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile
 from mainUi import Ui_main_window
+import threading
 
 
 class MainWindow(QMainWindow):
@@ -14,27 +15,33 @@ class MainWindow(QMainWindow):
 
         self.ui = Ui_main_window()
         self.ui.setupUi(self)
+        self.timer = QtCore.QTimer()
+
+        self.__stop_threads = True
+        self.click_thread = threading.Thread(target=self.click_inf, args=(1, "left", 2))
 
         self.ui.btn_stop.setDisabled(True)
         self.ui.btn_start.clicked.connect(self.start)
         self.ui.btn_stop.clicked.connect(self.stop)
 
-    def click(self, hold_time, repeat, mouse_btn, delay):
-        for _ in range(repeat):
+    def click_inf(self, hold_time, mouse_btn, delay):
+        while True:
             pyautogui.mouseDown(button=mouse_btn)
-            time.sleep(hold_time)
+            self.timer.start(hold_time * 1000)
             pyautogui.mouseUp(button=mouse_btn)
-            time.sleep(delay)
+            self.timer.start(delay * 1000)
+            if self.__stop_threads == True:
+                break
 
     def start(self):
         self.ui.btn_start.setDisabled(True)
         self.ui.btn_stop.setDisabled(False)
-        while self.ui.btn_start.isEnabled == False:
-            pyautogui.mouseDown(button="left")
-            QtCore.QTimer.start(1000)
-            pyautogui.mouseUp(button="left")
+        self.__stop_threads = False
+        self.click_thread.start()
 
     def stop(self):
+        self.__stop_threads = True
+        self.click_thread.join()
         self.ui.btn_start.setDisabled(False)
         self.ui.btn_stop.setDisabled(True)
 
