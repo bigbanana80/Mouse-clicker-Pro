9c26b93cc96223052bb81d0b1e27a84690a07bcb
Sepehr Aghajani <75833176+bigbanana80@users.noreply.github.com>
2024-04-30 21:45:18 +0330

    0.0.10 "timer works now"

--- main.py
+++ main.py
@@ -39,6 +39,11 @@ class MainWindow(QMainWindow):
         self.ui.le_s.setText("0")
         self.ui.le_ms.setText("100")
 
+        self.ui.le_timer_h.setText("0")
+        self.ui.le_timer_min.setText("0")
+        self.ui.le_timer_s.setText("0")
+        self.ui.le_timer_ms.setText("0")
+
         """        
         self.ui.le_hours.setDisabled(True)
         self.ui.le_mins.setDisabled(True)
@@ -55,6 +60,12 @@ class MainWindow(QMainWindow):
         self.ui.le_mins.textChanged.connect(self.update_vars)
         self.ui.le_s.textChanged.connect(self.update_vars)
         self.ui.le_ms.textChanged.connect(self.update_vars)
+
+        self.ui.le_timer_h.textChanged.connect(self.update_vars)
+        self.ui.le_timer_min.textChanged.connect(self.update_vars)
+        self.ui.le_timer_s.textChanged.connect(self.update_vars)
+        self.ui.le_timer_ms.textChanged.connect(self.update_vars)
+
         self.ui.btn_start.clicked.connect(self.start)
         self.ui.btn_stop.clicked.connect(self.stop)
 
@@ -79,7 +90,7 @@ class MainWindow(QMainWindow):
         else:
             self.stop()
 
-    def click_inf(self, hold_time, mouse_btn, delay):
+    def click_inf(self, hold_time, mouse_btn, delay, timer):
         if self.ui.comboB_mouse_btn.currentText() == "Left":
             down = self.MOUSEEVENTF_LEFTDOWN
             up = self.MOUSEEVENTF_LEFTUP
@@ -89,19 +100,41 @@ class MainWindow(QMainWindow):
         elif self.ui.comboB_mouse_btn.currentText() == "Middle":
             down = self.MOUSEEVENTF_MIDDLEDOWN
             up = self.MOUSEEVENTF_MIDDLEUP
+
+        if timer == 0:
+            while True:
+                ctypes.windll.user32.mouse_event(down, 0, 0, 0, 0)  # left down
+                time.sleep(hold_time)
+                ctypes.windll.user32.mouse_event(up, 0, 0, 0, 0)
+                time.sleep(delay)
+                if self.__stop_threads == True:
+                    return
+        else:
+            self.click_with_time(hold_time, delay, timer, up, down)
+
+    def click_with_time(self, hold_time, delay, timer, up, down):
+        self.thread_timer = threading.Thread(target=self.timer_countdown)
+        self.thread_timer.start()
         while True:
             ctypes.windll.user32.mouse_event(down, 0, 0, 0, 0)  # left down
             time.sleep(hold_time)
-            ctypes.windll.user32.mouse_event(up, 1, 0, 0, 0)
+            ctypes.windll.user32.mouse_event(up, 0, 0, 0, 0)
             time.sleep(delay)
             if self.__stop_threads == True:
-                break
+                self.ui.btn_start.setDisabled(False)
+                self.ui.btn_stop.setDisabled(True)
+                return
+
+    def timer_countdown(self):
+        temp = self.click_timer
+        time.sleep(temp)
+        self.__stop_threads = True
 
     def start(self):
         self.ui.btn_start.setDisabled(True)
         self.ui.btn_stop.setDisabled(False)
         self.click_thread = threading.Thread(
-            target=self.click_inf, args=(0, "left", self.click_speed)
+            target=self.click_inf, args=(0, "left", self.click_speed, self.click_timer)
         )
         self.__stop_threads = False
         self.click_thread.start()
@@ -118,6 +151,12 @@ class MainWindow(QMainWindow):
             + int(self.ui.le_mins.text()) * 60
             + int(self.ui.le_s.text())
             + int(self.ui.le_ms.text()) / 1000
+        )
+        self.click_timer = (
+            int(self.ui.le_timer_h.text()) * 3600
+            + int(self.ui.le_timer_min.text()) * 60
+            + int(self.ui.le_timer_s.text())
+            + int(self.ui.le_timer_ms.text()) / 1000
         )
 
 
